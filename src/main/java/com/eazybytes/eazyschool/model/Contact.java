59117552ac9fde3837faf1d05ad823007ee8ee2c
ARBFILE\awilson <awilson@arbfile.org>
2022-06-23 08:35:29 -0400

    upload files

---
+++ src/main/java/com/eazybytes/eazyschool/model/Contact.java
@@ -0,0 +1,46 @@
+package com.eazybytes.eazyschool.model;
+
+import lombok.Data;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.validator.constraints.Length;
+import org.springframework.stereotype.Component;
+;import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+@Entity
+@Table(name="contact_msg")
+@Data
+public class Contact extends BaseEntity{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
+    @GenericGenerator(name = "native", strategy = "native")
+    @Column(name = "contact_id")
+    private int contactId;
+
+    @NotBlank(message="Name must not be blank")
+    @Size(min=3, message="Name must be at least 3 characters long")
+    private String name;
+
+    @NotBlank(message = "Mobile number must not be blank")
+    @Pattern(regexp = "(^$|[0-9]{10})", message = "Mobile number must be 10 digits")
+    private String mobileNum;
+
+    @NotBlank(message = "Email must not be blank")
+    @Email(message = "Please provide a valid email address")
+    private String email;
+
+    @NotBlank(message = "Subject must not be blank")
+    @Size(min = 5, message = "Subject must be at least 5 characters long")
+    private String subject;
+
+    @NotBlank(message = "Message must not be blank")
+    @Size(min = 10, message = "Subject must be at least 10 characters long")
+    private String message;
+
+    private String status;
+
+
+}
